e8f910bfa28c219db5f0d87f38437b3b816c6d74
Twenty-August <twentytwentyaugust@outlook.com>
2021-04-07 13:26:34 +0900

    Implement of the useReducer hook

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useReducer } from 'react';
 import './Product.css';
 
 const products = [
@@ -19,64 +19,62 @@ const products = [
   }
 ];
 
-export default class Product extends Component {
-  state = {
-    cart: [],
-  }
+const currencyOptions = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+}
 
-  add = (product) => {
-    this.setState(state => ({
-      cart: [...state.cart, product],
-    }))
-  }
+function getTotal(cart) {
+  const total = cart.reduce((totalCost, item) => totalCost + item.price, 0);
+  return total.toLocaleString(undefined, currencyOptions)
+}
 
-  currencyOptions = {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }
-
-  getTotal = () => {
-    const total = this.state.cart.reduce((totalCost, item) => totalCost + item.price, 0);
-    return total.toLocaleString(undefined, this.currencyOptions)
-  }
-
-  remove = (product) => {
-    this.setState(state => {
-      const cart = [...state.cart];
-      const productIndex = cart.findIndex(p => p.name === product.name);
+function cartReducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      return [...state, action.product];
+    case 'remove':
+      const productIndex = state.findIndex(item => item.name === action.product.name);
       if (productIndex < 0) {
-        return;
+        return state;
       }
-      cart.splice(productIndex, 1)
-      return ({
-        cart
-      })
-    })
+      const update = [...state];
+      update.splice(productIndex, 1)
+      return update
+    default:
+      return state;
+  }
+}
+
+export default function Product() {
+  const [cart, setCart] = useReducer(cartReducer, []);
+
+  function add(product) {
+    setCart({ product, type: 'add' });
   }
 
-  componentDidUpdate() {
-    console.log(this.state)
+  function remove(product) {
+    setCart({ product, type: 'remove' });
   }
 
-  render() {
-    return (
-      <div className="wrapper">
-        <div>
-          Shopping Cart: {this.state.cart.length} total items.
-        </div>
-        <div>Total {this.getTotal()}</div>
-        <div>
-          {products.map(product => (
-            <div key={product.name}>
-              <div className="product">
-                <span role="img" aria-label={product.name}>{product.emoji}</span>
-              </div>
-              <button onClick={() => this.add(product)}>Add</button>
-              <button onClick={() => this.remove(product)}>Remove</button>
-            </div>
-          ))}
-        </div>
+  return (
+    <div className="wrapper">
+      <div>
+        Shopping Cart: {cart.length} total items.
       </div>
-    )
-  }
+      <div>Total: {getTotal(cart)}</div>
+
+      <div>
+        {products.map(product => (
+          <div key={product.name}>
+            <div className="product">
+              <span role="img" aria-label={product.name}>{product.emoji}</span>
+            </div>
+            <button onClick={() => add(product)}>Add</button>
+            <button onClick={() => remove(product)}>Remove</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
